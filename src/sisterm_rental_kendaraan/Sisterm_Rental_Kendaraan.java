f56b632786178a04037b614a452f885785be39e1
Abintaraa <soka@student.undiksha.ac.id>
2026-06-29 20:57:55 +0800

    Commit Tugas

---
+++ src/sisterm_rental_kendaraan/Sisterm_Rental_Kendaraan.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sisterm_rental_kendaraan;
+
+/**
+ *
+ * @author Abin Tara
+ */
+public class Sisterm_Rental_Kendaraan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
