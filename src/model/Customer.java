8501d1474edc3d6ac11a3ad40a7c5cfb41efc63e
vrilla147 <nikomangevrila2007@gmail.com>
2026-07-06 11:32:19 +0800

    Add files via upload

---
+++ src/model/Customer.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author evr
+ */
+public class Customer {
+    private int id;
+    private String nama;
+    private String nomorKTP;
+    private String jenisKelamin;
+    private String alamat;
+    private String nomorTelepon;
+    private String tanggalDaftar;
+
+    public Customer() {
+    }
+    
+    public Customer(String nama, String alamat, String noTelepon, String noKtp, String jenisKelamin, String tanggalDaftar) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.nomorTelepon = noTelepon;
+        this.nomorKTP = noKtp;
+        this.jenisKelamin = jenisKelamin;
+        this.tanggalDaftar = tanggalDaftar;
+    }
+
+    public int getId() { 
+        return id; 
+    }
+    
+    public void setId(int id) { 
+        this.id = id; 
+    }
+
+    public String getNama() { 
+        return nama; 
+    }
+    
+    public void setNama(String nama) { 
+        this.nama = nama; 
+    }
+    
+    public String getNomorKTP() { 
+        return nomorKTP; 
+    }
+    public void setNomorKTP(String nomorKTP) { 
+        this.nomorKTP = nomorKTP; 
+    }
+
+    public String getJenisKelamin() { 
+        return jenisKelamin; 
+    }
+    
+    public void setJenisKelamin(String jenisKelamin) { 
+        this.jenisKelamin = jenisKelamin; 
+    }
+
+    public String getAlamat() { 
+        return alamat; 
+    }
+    
+    public void setAlamat(String alamat) { 
+        this.alamat = alamat; 
+    }
+
+    public String getNomorTelepon() { 
+        return nomorTelepon; 
+    }
+    
+    public void setNomorTelepon(String nomorTelepon) { 
+        this.nomorTelepon = nomorTelepon; 
+    }
+
+    public String getTanggalDaftar() { 
+        return tanggalDaftar; 
+    }
+    
+    public void setTanggalDaftar(String tanggalDaftar) { 
+        this.tanggalDaftar = tanggalDaftar; 
+    }
+
+    public String getNoTelepon() {
+        return nomorTelepon;
+    }
+
+    public String getNoKtp() {
+        return nomorKTP;
+    }   
+    
+}
